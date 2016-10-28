e64d7ea393be3c049c726e2f6f1767c367f59fd5
Daniel Acedo Calderon <DanielExMachina0451@gmail.com>
2016-10-28 14:30:09 +0200

    Initial commit

---
+++ app/src/main/java/com/danielacedo/slidinguppaneltest/SlidingUpPanelTest.java
@@ -0,0 +1,13 @@
+package com.danielacedo.slidinguppaneltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlidingUpPanelTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sliding_up_panel_test);
+    }
+}
